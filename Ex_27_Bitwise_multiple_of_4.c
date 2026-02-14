5ebc26b84bdb32760c0912adde37350ed4a6fd0e
chetanr-25 <chetan.r.sot25@pwioi.com>
2026-02-14 22:12:04 +0530

    Update Ex_27_Bitwise_multiple_of_4.c

--- Ex_27_Bitwise_multiple_of_4.c
+++ Ex_27_Bitwise_multiple_of_4.c
@@ -2,7 +2,7 @@
 Write a program to check if a given integer n is a multiple of 4 using only bitwise operators. You are not allowed to use the modulus operator (%), division operator (/), or loops. Use bitwise operations to determine the result efficiently.
 
 Input Format
-
+ 
 A single integer n.
 Constraints
 
